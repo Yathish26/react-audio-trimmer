4cec603b5fad5f8fde20b962ea8ce13a906cfdb4
Yathish Acharya <yathishkumar2013@gmail.com>
2024-10-07 18:32:17 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 import './App.css';
-import { TypographyH2, TypographyP } from './components/typography'; 
+import { TypographyH2, TypographyP } from './components/typography';
 import {
   Button,
   Container,
@@ -10,7 +10,7 @@ import {
   Box,
   Paper,
   LoadingOverlay,
-} from '@mantine/core'; 
+} from '@mantine/core';
 import { Region, WaveForm, WaveSurfer } from 'wavesurfer-react';
 import TimelinePlugin from 'wavesurfer.js/dist/plugins/timeline';
 import RegionsPlugin from 'wavesurfer.js/dist/plugins/regions';
@@ -28,6 +28,11 @@ function App() {
   const wavesurferRef = useRef(null);
   const [loaded, setLoaded] = useState(false);
   const ffmpegRef = useRef(new FFmpeg());
+  const [startTime, setStartTime] = useState(0);
+  const [endTime, setEndTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  
+
 
   const load = async () => {
     const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.6/dist/esm';
@@ -82,12 +87,19 @@ function App() {
       id: 'region-1',
       start: 0,
       end: 15,
-      color: 'rgba(98, 86, 202, .5)',
+      color: 'rgba(124, 0, 255, 0.4)',
+      drag: true,
+      resize: true,
+      handleStyle: {
+        left: 'rgba(98, 86, 202, 0.8)',
+        right: 'rgba(98, 86, 202, 0.8)',
+      },
       data: {
         systemRegionId: 33,
       },
     },
   ]);
+  
   const regionsRef = useRef(regions);
 
   useEffect(() => {
@@ -127,8 +139,18 @@ function App() {
     [regionCreatedHandler, regions]
   );
 
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+  };
+
+
   const handleRegionUpdate = useCallback((region) => {
     setRegionData(region);
+    setStartTime(formatTime(region.start));
+    setEndTime(formatTime(region.end));
+    setDuration(formatTime(region.end - region.start));
     const start = region.start;
     wavesurferRef.current.seekTo(start / wavesurferRef.current.getDuration());
     wavesurferRef.current.play();
@@ -196,104 +218,171 @@ function App() {
       withGlobalStyles
       withNormalizeCSS
     >
-      <Container size="md" mt="xl">
-        <Paper shadow="lg" p="xl" radius="md" style={{ backgroundColor: '#f0e6ff' }}>
-          {!audio ? (
-            <Box
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                minHeight: '300px',
-                padding: '20px',
-                border: '1px solid #CDC1FF',
-                borderRadius: '8px',
-                backgroundColor: '#F0F0FF',
-              }}
-            >
-              <img style={{ width: '150px' }} src="src/assets/wave.svg" alt="" />
-              <TypographyH2 text="Upload Audio" />
-              <Text size="md" mt="sm" mb="lg" align="center">
-                Select an audio file to upload and trim the audio as needed.
-              </Text>
-              <Button
-                onClick={() => fileInputRef.current.click()}
-                size="lg"
-                variant="filled"
-                color="violet"
-                radius="xl"
+      <div
+        style={{
+          minHeight: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          background: 'linear-gradient(135deg, rgb(124 0 255), rgb(237, 231, 246))',
+        }}
+      >
+        <Container size="md" mt="xl">
+          <Paper
+            shadow="xl"
+            p="xl"
+            radius="lg"
+            style={{
+              background: 'linear-gradient(135deg, rgb(132 15 255);',
+              border: '1px solid #E6E6FA',
+              boxShadow: '0 10px 30px rgba(0, 0, 0, 0.1)',
+              maxWidth: '600px',
+              margin: 'auto',
+            }}
+          >
+            {!audio ? (
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  minHeight: '350px',
+                  padding: '25px',
+                  border: '2px dashed #CDC1FF',
+                  borderRadius: '16px',
+                  background: 'linear-gradient(135deg, #f7f7ff, #ececff)',
+                }}
               >
-                Upload Audio
-              </Button>
-              <input
-                onChange={handleChange}
-                multiple={false}
-                ref={fileInputRef}
-                type="file"
-                accept="audio/*"
-                style={{ display: 'none' }}
-              />
-            </Box>
-
-          ) : (
-            <Box
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                minHeight: '400px', 
-                padding: '20px',
-                border: '1px solid #CDC1FF',
-                borderRadius: '8px',
-                backgroundColor: '#F0F0FF', 
-              }}
-            >
-              <TypographyH2 text="Trim Audio" />
-              <TypographyP text={`Audio file: ${audio.name}`} align="center" />
-
-              <Box mt="md" sx={{ width: '100%' }}>
-                <WaveSurfer
-                  plugins={plugins}
-                  onMount={handleWSMount}
-                  cursorColor="transparent"
-                  container="#waveform"
-                  url={audioUrl}
-                  waveColor={'#A594F9'}
-                  barHeight={0.8}
-                  progressColor={'#CDC1FF'}
-                  barWidth={2}
-                  dragToSeek={false}
-                  autoplay={true}
-                  interact={false}
+                <img
+                  style={{ width: '120px', opacity: 0.9 }}
+                  src="src/assets/wave.svg"
+                  alt="Audio Waveform"
+                />
+                <TypographyH2
+                  text="Upload Audio"
+                  style={{ fontWeight: 'bold', color: '#7b1fa2' }}
+                />
+                <Text
+                  size="md"
+                  mt="sm"
+                  mb="lg"
+                  align="center"
+                  style={{ color: '#7a7a7a' }}
                 >
-                  <WaveForm id="waveform" />
-                  <div id="timeline" />
-                  {isLoaded &&
-                    regions.map((regionProps) => (
-                      <Region
-                        onUpdateEnd={handleRegionUpdate}
-                        key={regionProps.id}
-                        {...regionProps}
-                      />
-                    ))}
-                </WaveSurfer>
-
-                <Group position="center" mt="xl">
-                  <Button onClick={play} size="lg" variant="filled" color="violet" radius="xl">
-                    {isPlaying ? 'Pause' : 'Play'}
-                  </Button>
-                  <Button onClick={trim} size="lg" variant="filled" color="violet" radius="xl">
-                    Trim Audio
-                  </Button>
-                </Group>
+                  Select an audio file to upload and trim the audio as needed.
+                </Text>
+                <Button
+                  onClick={() => fileInputRef.current.click()}
+                  size="lg"
+                  variant="gradient"
+                  gradient={{ from: 'violet', to: 'purple' }}
+                  radius="xl"
+                  sx={{ boxShadow: '0 4px 12px rgba(146, 118, 255, 0.5)' }}
+                >
+                  Upload Audio
+                </Button>
+                <input
+                  onChange={handleChange}
+                  multiple={false}
+                  ref={fileInputRef}
+                  type="file"
+                  accept="audio/*"
+                  style={{ display: 'none' }}
+                />
               </Box>
-            </Box>
-          )}
-        </Paper>
-      </Container>
+            ) : (
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  minHeight: '450px',
+                  padding: '25px',
+                  border: '2px solid #CDC1FF',
+                  borderRadius: '16px',
+                  background: 'linear-gradient(135deg, #f0f0ff, #eaeaff)',
+                }}
+              >
+                <TypographyH2
+                  text="Trim Audio"
+                  style={{ fontWeight: 'bold', color: '#7b1fa2' }}
+                />
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                  <img style={{ width: '20px', opacity: 0.9 }} src="src/assets/music.svg" alt="" />
+                  <TypographyP text={`${audio.name}`} align="center" style={{ color: '#6a1b9a' }} />
+                </div>
+
+                <Box mt="md" sx={{ width: '100%' }}>
+                  <WaveSurfer
+                    plugins={plugins}
+                    onMount={handleWSMount}
+                    cursorColor="transparent"
+                    container="#waveform"
+                    url={audioUrl}
+                    waveColor={'#A594F9'}
+                    barHeight={0.5}
+                    progressColor={'#CDC1FF'}
+                    barWidth={3}
+                    dragToSeek={true}
+                    autoplay={true}
+                    interact={false}
+                  >
+                    <WaveForm id="waveform" />
+                    <div id="timeline" />
+                    {isLoaded &&
+                      regions.map((regionProps) => (
+                        <Region
+                          onUpdateEnd={handleRegionUpdate}
+                          key={regionProps.id}
+                          {...regionProps}
+                        />
+                      ))}
+                  </WaveSurfer>
+                  <Group position="center" mt="md" justify="center">
+                    <Text size="sm" color="dimmed">
+                      Start Time: {startTime} seconds
+                    </Text>
+                    <Text size="sm" color="dimmed">
+                      End Time: {endTime} seconds
+                    </Text>
+                    <Text size="sm" color="dimmed">
+                      Duration: {duration} seconds
+                    </Text>
+                  </Group>
+
+                  <Group position="center" mt="xl" justify="center">
+                    <Button
+                      onClick={play}
+                      size="lg"
+                      variant="gradient"
+                      gradient={{ from: 'violet', to: 'purple' }}
+                      radius="xl"
+                      sx={{ boxShadow: '0 4px 12px rgba(146, 118, 255, 0.5)' }}
+                    >
+                      Play / Pause
+                    </Button>
+                    <Button
+                      onClick={trim}
+                      size="lg"
+                      variant="gradient"
+                      gradient={{ from: 'violet', to: 'purple' }}
+                      radius="xl"
+                      sx={{ boxShadow: '0 4px 12px rgba(146, 118, 255, 0.5)' }}
+                    >
+                      Trim Audio
+                    </Button>
+                  </Group>
+                </Box>
+              </Box>
+            )}
+          </Paper>
+        </Container>
+      </div>
     </MantineProvider>
+
+
   );
 }
 
